0ba4f1025aaa0fde41f46f0da4595528a7035f66
seo0yoon <genious921225@gmail.com>
2022-08-28 23:34:00 +0900

    Modify: login add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Router from './Router';
 import GlobalStyle from './styles/GlobalStyle';
-
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import { createRoot } from 'react-dom/client';
